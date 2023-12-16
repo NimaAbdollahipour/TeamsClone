9e3517c0cb2a20e7d4d65729eb3d02c8391959d9
Nima Abdollahipour <nima.abdollahi.a@gmail.com>
2023-12-16 20:46:15 +0200

    creating models

--- teams_clone/teams/models.py
+++ teams_clone/teams/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    details = models.TextField()
+    department = models.CharField(max_length = 64)
+    role = models.CharField(max_length = 16)
+
+
+class Team(models.Model):
+    name = models.CharField(max_length = 64)
+    members = models.ManyToManyField(UserProfile, related_name='joined_teams')
+    owners = models.ManyToManyField(UserProfile, related_name='owned_teams')
+
+class Channel(models.Model):
+    name = models.CharField(max_length = 64)
+    team_id = models.ForeignKey(Team, on_delete=models.CASCADE)
+
+class Message(models.Model):
+    date_created = models.DateTimeField()
+    content = models.TextField()
+    sender = models.OneToOneField(User, on_delete=models.CASCADE)
+
+class PrivateMessage(Message):
+    receiver = models.OneToOneField(User, on_delete=models.CASCADE)
+
+class ChannelMessage(Message):
+    channel = models.OneToOneField(Channel, on_delete=models.CASCADE)
+
+class GroupMessage(Message):
+    receiver = models.OneToOneField(User, on_delete=models.CASCADE)
+
